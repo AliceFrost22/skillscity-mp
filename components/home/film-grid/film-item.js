ecc9049d8bf2780693ce61d3cc9f991aeaf3bd74
Alice Frost <alicefrost2@gmail.com>
2023-02-08 20:30:41 +0000

    addtional films added

--- components/home/film-grid/film-item.js
+++ components/home/film-grid/film-item.js
@@ -11,27 +11,31 @@ export default function FilmItem({ film }){
   }
 
   return(
-    <div id="film1" className="bg-amber-400 gap-5 ml-20 border border-black rounded-lg ">
+    <div id="film1" >
+      <div className="bg-amber-400 gap-5 ml-20 border border-black rounded-lg">
       <p className="mb-5 p-10 flex justify-center rounded-lg" >
         <img src={film.imgUrl}/>
         </p>
-      <p className="mb-5 ml-10 text-center font-medium max-w-6xl mx-auto">
-        {film.title}
-      </p>
-      <p className="mb-5 ml-10 text-center font-medium" >
-        {film.director}
-      </p>
-      <button onClick={(handleLike)}
-        className="bg-red-700 px-2 py-1 rounded text-white text-center">
-        Like {likes}
-      </button>
-      <CopyToClipboard text={film.linkToPurchase}>
-                <button
-                className="bg-red-900 px-4 rounded-lg text-center"
-                type="button" >
+      </div>
+        <div className="bg-amber-400 text-center gap-5 ml-20 mt-5 border border-black ">
+          <p className="text-center font-medium max-w-6xl mx-auto px-5 pt-5 pb-10">
+            {film.title}
+          </p>
+          <p className="font-medium" >
+            {film.director}
+          </p>
+        <button onClick={(handleLike)}
+          className="bg-red-700 px-2 py-1 rounded text-white text-center">
+            Like {likes}
+        </button>
+          <CopyToClipboard text={film.linkToPurchase}>
+            <button
+              className="bg-red-900 px-4 rounded-lg text-center"
+              type="button" >
                 Copy Link to Amazon
-                </button>
-                </CopyToClipboard>
-    </div>
+            </button>
+          </CopyToClipboard>
+      </div>
+   </div> 
   )
 }
--- components/home/navigation-bar/navigation-item.js
+++ components/home/navigation-bar/navigation-item.js
@@ -1,6 +1,6 @@
 export default function NavigationItem(props){
     return(
-      <div className = "rounded-lg px-3 py-1 text-center">
+      <div className = "rounded-lg px-3 py-1 text-center gap-5">
         <a href={props.ahref}>{props.name}</a>
       </div>
     )
