07dea21d6287ff26057911170a3174536571d67b
Alice Frost <alicefrost2@gmail.com>
2023-02-15 15:26:56 +0000

    mobile responsive added

--- components/home/header/banner.js
+++ components/home/header/banner.js
@@ -1,7 +1,7 @@
 export default function Banner(){
     return(
         <div className="w-full">
-            <div className="w-full flex justify-center h-48 mt-5 bg-lime-200"> <img src="/assets/Alice-w-bg.png/"/>   
+            <div className="w-full flex justify-center h-48 mt-5"> <img src="/assets/Alice-w-bg.png/"/>   
            </div>
         </div>
     )
