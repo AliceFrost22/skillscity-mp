9d75e0c199601d205584e5311baaba38fa3d83d2
AliceFrost22 <alicefrost2@gmail.com>
2023-02-23 11:53:39 +0000

    neatened

--- components/home/navigation-bar/search-bar.js
+++ components/home/navigation-bar/search-bar.js
@@ -3,7 +3,7 @@ import React from "react";
 const SearchPage = () => {
   return (
     <form className="w-full flex items-center md:justify-end md:pr-10 md:form-inline mt-4 mb-4 w-100">
-         <input className="form-control form-control-sm ml-3 rounded-md" type="text" placeholder="Search" aria-label="Search" />
+         <input className="form-control form-control-sm ml-3 rounded-md ml-4" type="text" placeholder="Search" aria-label="Search" />
       </form>
   );
 }
